99c0720dd0dacf073fd8f79e0d8f549bb3c8007a
Leonardo Maciel Deschamps <leonardo@unidavi.edu.br>
2018-11-06 19:44:20 -0200

    update

---
+++ app/Http/Controllers/ProcessoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ProcessoController extends Controller {
+    private $oModel;
+    private $oView;
+
+    public function getRegistros(){
+        echo 1;
+    }
+
+}
